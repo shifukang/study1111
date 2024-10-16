2ddaf2990e86fc97dcdffaa3a2cab464b9de0871
zhang666677778 <zhang666677778@163.com>
2024-10-16 22:55:37 +0800

    day1 2

---
+++ day1/Solution.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+	int search(std::vector<int>& nums, int target);
+	int removeElement(std::vector<int>& nums, int val);
+};
--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,20 +1,56 @@
-﻿// day1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include "Solution.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<int> vec = { 1,2,3,4,5,6 };
+    int target = 4;
+    Solution searchSolution;
+    int result = searchSolution.search(vec, target);
+    std::cout << result << std::endl;
+
+    std::vector<int> vec1 = { 1,2,2,2,2,2,2,2,2,2,3,4,5,6,2,2,2 };
+    int removeTarget = 2;
+    int resultLength = searchSolution.removeElement(vec1, removeTarget);
+    std::cout << resultLength << std::endl;
+
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+    int Solution::search(std::vector<int>& nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] < target)
+            {
+                left = middle + 1;
+            }
+            else if (nums[middle] > target)
+            {
+                right = middle - 1;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+        return -1;
+
+    }
+    int Solution::removeElement(std::vector<int>& nums, int val)
+    {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++)
+        {
+            if (nums[fastIndex] != val)
+            {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+
